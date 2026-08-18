bdf411b6a95f4e9ea2bc595abd6ce59d78aa913e
Anurag Kumar <anuragakn18@gmail.com>
2026-08-18 20:12:45 +0530

    completed setup for database(model, collections, schema)

--- day-09/src/app.js
+++ day-09/src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const userModel = require('./models/user.model');
 
 const app = express();
 
 app.use(express.json());
 
-
+userModel.create()
 module.exports = app;
